36da4bd33108f67ec727da1660aaa683b9a34651
jhluc <jhluc@uci.edu>
2024-01-29 15:30:41 -0800

    settings UI 1/3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
-from PyQt6.QtWidgets import QHBoxLayout,QStackedLayout,QLabel, QLineEdit  ,QGridLayout ,QApplication,QMainWindow, QWidget, QLabel, QCalendarWidget ,QVBoxLayout
+from PyQt6.QtWidgets import QHBoxLayout,QComboBox,QStackedLayout,QLabel, QLineEdit  ,QGridLayout ,QApplication,QMainWindow, QWidget, QLabel, QCalendarWidget ,QVBoxLayout
 from PyQt6.QtGui import QIcon, QFont
 
 # Subclass QMainWindow to customize your application's main window
@@ -36,28 +36,77 @@ class MainWindow(QMainWindow):
 
         addeventbutton = QPushButton("Add Event")
         addeventbutton.setStyleSheet('color:green')
-
+        addeventbutton.pressed.connect(self.reveal_settings_add)
         removeeventbutton = QPushButton("Remove Event")
         removeeventbutton.setStyleSheet('color:green')
-
+        removeeventbutton.pressed.connect(self.reveal_settings_remove)
         updateeventbutton = QPushButton("Update Event")
         updateeventbutton.setStyleSheet('color:green')
+        updateeventbutton.pressed.connect(self.reveal_settings_update)
 
         settings_layout.addWidget(addeventbutton)
         settings_layout.addWidget(removeeventbutton)
         settings_layout.addWidget(updateeventbutton)
-
-        stacked_layout = QStackedLayout()
+        self.settings_date_selection = QLabel("No Date Selected")
+        self.calendar.selectionChanged.connect(self.calendar_date_slot)
+        settings_layout.addWidget(self.settings_date_selection)
+        self.stacked_layout = QStackedLayout()
         #here is where u add layouts for each function to the stacked_layout 
-        #self.settings_add_event_ = QWidget() then add a layout to taht QWidget and thats gonna be the settings for each button
-        settings_layout.addLayout(stacked_layout)
 
+        #add event widgit and layout 
+        settings_add_event_ = QWidget() #then add a layout to taht QWidget and thats gonna be the settings for each button
+        add_event_layout = QGridLayout()
+        add_event_layout.setSpacing(0)
+        settings_add_event_.setLayout(add_event_layout)
+        self.calendar.selectionChanged.connect(self.calendar_date_slot)
+        hour_combo_box = QComboBox()
+        hour_combo_box.setFixedWidth(100)
+        hour_combo_box.addItems([str(i) for i in range(24)])
+        hour_combo_box_label = QLabel("set your event start hour: ")
+        add_event_layout.addWidget(hour_combo_box_label, 0,0)
+        add_event_layout.addWidget(hour_combo_box, 0,1)
+        min_combo_box_label = QLabel("set your event start minute: ")
+        min_combo_box = QComboBox()
+        min_combo_box.addItems([str(i) for i in range(60)])
+        add_event_layout.addWidget(min_combo_box_label)
+        add_event_layout.addWidget(min_combo_box)
 
-    
-        stacked_layout.addWidget(QLineEdit())
+        hour_combo_boxe = QComboBox()
+        hour_combo_boxe.addItems([str(i) for i in range(24)])
+        hour_combo_box_labele = QLabel("set your event end hour: ")
+        add_event_layout.addWidget(hour_combo_box_labele)
+        add_event_layout.addWidget(hour_combo_boxe)
+        min_combo_box_labele = QLabel("set your event end minute: ")
+        min_combo_boxe = QComboBox()
+        min_combo_boxe.addItems([str(i) for i in range(60)])
+        add_event_layout.addWidget(min_combo_box_labele)
+        add_event_layout.addWidget(min_combo_boxe)
+        add_event_button = QPushButton()
+        add_event_button.setText("Add Event")
+        add_event_layout.addWidget(add_event_button)
+        self.stacked_layout.addWidget(settings_add_event_)
 
+        #remove event QWidgit 
+        settings_remove_event = QWidget()
+        remove_event_layout = QGridLayout()  
+        settings_remove_event.setLayout(remove_event_layout)  
+        remove_label = QLabel("NOT IMPLEMENTED")
+        remove_event_layout.addWidget(remove_label,0,0)
+        self.stacked_layout.addWidget(settings_remove_event)
 
+        #update event QWidgit
+        settings_update_event = QWidget()
+        update_event_layout = QGridLayout()  
+        settings_update_event.setLayout(update_event_layout)  
+        update_label = QLabel("NOT IMPLEMENTED for update")
+        update_event_layout.addWidget(update_label,0,0)
+        self.stacked_layout.addWidget(settings_update_event)
+
+ 
+
+        settings_layout.addLayout(self.stacked_layout)
         calendar_layout.addLayout(settings_layout,1,2)
+        #calendar_layout.addWidget(settings_add_event_,2,2)
     
         main_widget = QWidget()
         main_widget.setLayout(calendar_layout)
@@ -70,11 +119,22 @@ class MainWindow(QMainWindow):
         date_in_string = str(date_class.toString())
         #just pass in the QDate to the event class because it has relevant getters
         self.label.setText("Selected Date Is : " + date_in_string)
+        self.settings_date_selection.setText("Selected Date Is : " + date_in_string)
+
+    def reveal_settings_add(self):
+        self.stacked_layout.setCurrentIndex(0)
+
+    def reveal_settings_remove(self):
+        self.stacked_layout.setCurrentIndex(1)
+
+    def reveal_settings_update(self):
+        self.stacked_layout.setCurrentIndex(2)
  
  
  
  
 app = QApplication(sys.argv)
+
 window = MainWindow()
 window.show()
 sys.exit(app.exec())
