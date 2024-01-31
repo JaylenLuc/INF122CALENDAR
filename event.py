84779a529cbf38bc745659ada6ba4a74813472e4
jhluc <jhluc@uci.edu>
2024-01-30 19:06:00 -0800

    some bgu fixes

--- event.py
+++ event.py
@@ -4,10 +4,10 @@ class Event:
     def __init__(self, title : str, startHour: int, endHour: int, startMin: int, endMin: int,
                     date_string):
         self.title = title
-        self.starthour = startHour
-        self.endhour = endHour
-        self.startmin = startMin
-        self.endmin = endMin
+        self.starthour =  "0" + startHour if len(startHour) == 1 else startHour
+        self.endhour = "0" + endHour if len(endHour) == 1 else endHour
+        self.startmin = "0" + startMin if len(startMin) == 1 else startMin
+        self.endmin = "0" + endMin if len(endMin) == 1 else endMin
         self.date_string = date_string
     
     def __str__(self):
